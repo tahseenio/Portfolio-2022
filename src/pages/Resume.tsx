79d4f6542651f8659778915287fee37a85c02a14
Tahseen <tahseenislam@hotmail.com>
2022-06-04 20:20:22 +1000

    added pdf

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,10 +1,16 @@
 // TODO: figure out how to generate resume this based on reading my current pdf.
 
+import { Document, Page } from 'react-pdf';
+
+const url = 'something';
+
 const Resume = () => {
   return (
     <section className='container'>
       <div className='row'>
-        <p>Resume</p>
+        <Document file={url}>
+          <Page pageNumber={1} />
+        </Document>
       </div>
     </section>
   );
