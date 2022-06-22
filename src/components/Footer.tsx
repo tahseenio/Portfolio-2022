9e46fc01188ea809e7c24a919ea521212e4a8cce
Tahseen <tahseenislam@hotmail.com>
2022-06-22 15:06:42 +1000

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,83 @@
 // TODO: what will footer include?
 // TODO: get an idea for Footer UI
 
-import { useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+import { AiOutlineArrowUp } from 'react-icons/ai';
+
+import footerLogo from '../assets/logo_inverted.svg';
+import { usePortfolioContext } from '../context/PortfolioContext';
 
 const Footer = () => {
+  const { HomeRef, AboutRef, ProjectsRef, ResumeRef, ContactRef } =
+    usePortfolioContext();
+
+  const links = [
+    { title: 'Home', reference: HomeRef, id: '1' },
+    { title: 'About', reference: AboutRef, id: '2' },
+    { title: 'Projects', reference: ProjectsRef, id: '3' },
+    { title: 'Resume', reference: ResumeRef, id: '4' },
+    { title: 'Contact', reference: ContactRef, id: '5' },
+  ];
+
+  const socialLinks = [
+    { title: 'LinkedIn', link: 'https://www.linkedin.com/in/tahseen1/' },
+    { title: 'Github', link: 'https://github.com/tahseenio/' },
+  ];
+
+  const linkVariants = {
+    hover: { scale: 1.1, rotate: 5 },
+    tap: { scale: 0.9 },
+  };
+
   return (
     <footer>
+      <div className='footer--top'>
+        <ul className='footer__links'>
+          {links.map(({ title, id, reference }) => (
+            <motion.li
+              key={id}
+              onClick={() => {
+                reference?.current!.scrollIntoView();
+              }}
+              className='footer__link'
+              variants={linkVariants}
+              whileHover='hover'
+              whileTap='tap'
+            >
+              {title}
+            </motion.li>
+          ))}
+        </ul>
+        <figure
+          className='footer__figure'
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          <img src={footerLogo} className='footer__logo' alt='footer logo' />
+          <span className='footer__logo--popper'>
+            Top <AiOutlineArrowUp />
+          </span>
+        </figure>
+        <ul className='footer__links'>
+          {socialLinks.map(({ title, link }, index) => (
+            <motion.li
+              className='footer__link'
+              variants={linkVariants}
+              whileHover='hover'
+              whileTap='tap'
+            >
+              <a
+                key={index}
+                className='footer__link--anchor'
+                href={link}
+                target='_blank'
+              >
+                {title}
+              </a>
+            </motion.li>
+          ))}
+        </ul>
+      </div>
       <p className='footer__description'>Copyright © 2022 By Tahseen Islam</p>
     </footer>
   );
