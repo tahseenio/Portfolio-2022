c5b62bd2b2d41dd2168e6bd50a9431601f343d29
Tahseen <tahseenislam@hotmail.com>
2022-07-18 21:49:12 +1000

    removed fake load

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,30 @@ import HomeNightMobile from './assets/homeNightMobile.webp';
 function App() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const imagesPreload = [HomeBackground, HomeNight, HomeNightMobile];
-    const fetchImages = () => {
-      // console.log('image fetch started');
-      Promise.all(
-        imagesPreload.map((url) => {
-          const newImage = new Image();
-          newImage.src = url;
-          return new Promise((res) => {
-            newImage.onload = () => {
-              newImage.decode().then(() => res(url));
-            };
-          });
-        })
-      ).then((item) => {
-        setTimeout(() => {
-          setLoading(false);
-          // console.log('DONE', item);
-        }, 500);
-      });
-    };
-    fetchImages();
+    setTimeout(() => {
+      setLoading(false);
+    }, 300);
+    // const imagesPreload = [HomeBackground, HomeNight, HomeNightMobile];
+    // const fetchImages = () => {
+    //   // console.log('image fetch started');
+    //   Promise.all(
+    //     imagesPreload.map((url) => {
+    //       const newImage = new Image();
+    //       newImage.src = url;
+    //       return new Promise((res) => {
+    //         newImage.onload = () => {
+    //           newImage.decode().then(() => res(url));
+    //         };
+    //       });
+    //     })
+    //   ).then((item) => {
+    //     setTimeout(() => {
+    //       setLoading(false);
+    //       // console.log('DONE', item);
+    //     }, 500);
+    //   });
+    // };
+    // fetchImages();
   }, []);
 
   return (
